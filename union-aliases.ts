79b248aa82b20993e352f99a65943abc537d3464
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-06 10:28:40 +1000

    union types, literal types and aliases

---
+++ union-aliases.ts
@@ -0,0 +1,28 @@
+type Combinable = number | string;
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: 'as-number'| 'as-text'
+) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  if (resultConversion === "as-number") {
+    return +result;
+  } else {
+    return result.toString();
+  }
+}
+
+const combindeAges = combine(30, 26, "as-number");
+console.log(combindeAges);
+
+const combineAgesString = combine("30", "26", "as-number");
+console.log(combineAgesString);
+
+const combineNames = combine("max", "anna", "as-text");
+console.log(combineNames);
