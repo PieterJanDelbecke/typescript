4eb3227fa3ce6957cbce525ff97dd6e06243b012
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-06 14:25:49 +1000

    the unknown type

---
+++ functions.ts
@@ -0,0 +1,15 @@
+function add(n1: number, n2: number){
+    return n1 + n2
+}
+
+function printResult(num: number){
+    console.log("Result: " + num);
+}
+
+
+let combineValues: (a: number, b: number) => number
+
+combineValues = add;
+// combineValues = printResult;
+
+console.log(combineValues(8,8))
