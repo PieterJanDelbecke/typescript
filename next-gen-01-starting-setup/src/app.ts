6791e6cfd10918913ad8a93ea6a6e760e41b8315
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-08 14:24:04 +1000

    the rest operator

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+
+console.log(addedNumbers)
